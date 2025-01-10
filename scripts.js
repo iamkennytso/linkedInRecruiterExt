372e388b6198a4a66c4910fd3aacbf1c683c6a8a
Kenneth Tso <iamkennytso@gmail.com>
2025-01-10 00:28:52 -0500

    expand things

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
-window.onload = function(){
+window.onload = function() {
+  const heroBanner = document.querySelector('.profile-background-image')
+  heroBanner.style.display = 'none';
+
+  setTimeout(() => {
+    const premiumAd = document.querySelector('.pv-top-card__premium-feature-banner');
+    if (premiumAd) {
+      premiumAd.style.display = 'none';
+    }
+  }, 1500)
+
   const hideIds = ['highlights', 'featured', 'content_collections']
   hideIds.forEach(id => {
     const hideElement = setInterval(function() {
@@ -12,4 +22,19 @@ window.onload = function(){
       }
     }, 500)
   })
+
+  const expandList = ['experience', 'about']
+  expandList.forEach(id => {
+    setInterval(function() {
+      const idDiv = document.querySelector(`#${id}`)
+      if (idDiv) {
+        setTimeout(() => {
+          const idContainer = idDiv.parentElement
+          const buttons = idContainer.querySelectorAll('button')
+          buttons.forEach(button => button.click())
+        }, 500)
+      }
+    })
+  })
+
 }
